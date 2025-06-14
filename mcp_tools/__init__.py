1ec522474fa174eccde9ecd06292a3a782923837
felipe-silvieri-braslive <felipesilvieri@braslive.com>
2025-06-14 19:12:42 -0300

    add logo

--- mcp_tools/__init__.py
+++ mcp_tools/__init__.py
@@ -3,7 +3,10 @@ from fastmcp import FastMCP
 from .generate_quote import GenerateQuoteTool
 
 
-mcp = FastMCP("Quotation Generator")
+mcp = FastMCP(
+    name="Quotation Generator",
+    description="Uma tool que orquestra a geração de orçamentos para clientes.",
+)
 mcp.add_tool(GenerateQuoteTool())
 
 __all__ = ["mcp", "GenerateQuoteTool"]
